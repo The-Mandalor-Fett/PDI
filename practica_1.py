cbee7cc2b2cfcceb771681a35e2b169b8ac60166
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-03-04 11:16:30 -0600

    Agregamos la apertura de la imagen

---
+++ practica_1.py
@@ -0,0 +1,14 @@
+from re import I
+from PIL import Image
+
+def abrir(img):
+
+    #EL NOMBRE DE LA IMAGEN SERA INTRODUCIDA EN LA VARIABLE img
+    #POR ESO SE CONCATENA
+    rutaArchivo=("./img/"+img) 
+
+    img= Image.open(rutaArchivo) #ABRE LA IMAGEN DESDE LA RUTA OBTENIDA
+
+    img.show() #MUESTRA LA IMAGEN EN TU EXPLORADOR DEFALUT
+    print(rutaArchivo)#MUESTRA LA RUTA DEL ARCHIVO
+    
