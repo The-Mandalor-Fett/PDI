7ce899aa13e680be19f746ff751c76bf7792675b
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-03-05 15:13:28 -0600

    Agregamos la parte que cambia de color a grises

--- practica_1.py
+++ practica_1.py
@@ -1,7 +1,8 @@
 from re import I
+from sre_parse import WHITESPACE
 from PIL import Image
 
-def abrir(img):
+def abrir(img):#SE LE PASA EL nombre DE LA IMAGEN COMO PARÁMETRO
 
     #EL NOMBRE DE LA IMAGEN SERA INTRODUCIDA EN LA VARIABLE img
     #POR ESO SE CONCATENA
@@ -11,4 +12,33 @@ def abrir(img):
 
     img.show() #MUESTRA LA IMAGEN EN TU EXPLORADOR DEFALUT
     print(rutaArchivo)#MUESTRA LA RUTA DEL ARCHIVO
+    
+#COMO NO QUIERO TRABAJAR CON OTRA IMAGEN EN GRISES CONVERTIRÉ LA IMAGEN ORIGINAL EN GRISES
+def escala_grises(img): #SE LE PASA EL nombre DE LA IMAGEN COMO PARÁMETRO
+
+    rutaArchivo=("./img/"+img) 
+    img= Image.open(rutaArchivo) 
+    img.show() #MUESTRA LA IMAGEN EN TU EXPLORADOR DEFALUT
+    #AHORA NECESITAMOS CREAR UNA NUEVA VARIABLE QUE CONTENGA LA NUEVA IMAGEN EN
+    #ESCALA DE GRISES, LO HAREMOS CAMBIANDO CADA PIXEL DE LA IMAGEN CON SU CORRESPONDIENTE EN RGB
+    #Y SACANDO SU PROMEDIO
+    imgris = img
+    i = 0
+    while i < imgris.size[0]: #.size ES PARA EL TAMAÑO DEL arreglo Y [0] ES PARA LAS FILAS
+        j = 0
+        while j < imgris.size[1]:
+            r, g , b = imgris.getpixel((i,j)) #OBTIENE EL PIXEL DE LA POSICIÓN [i,j]
+            
+            #PARA OBTENER LA IMAGEN EN GRISES DEBAMOS SACAR SU PORMEDIO COMO ENTERO
+            gris = int((r + g + b)/3)
+            #PARA PONER EL PIXEL EN GRISES NECESITAMOS UNA TUPLA QUE CONTENGA LOS 3 NIVELES DE GRIS
+            pixel = tuple([gris,gris,gris])#GUARDAMOS LOS VALORES DE GRIS EN UNA TUPLA
+            print(pixel)
+            imgris.putpixel((i,j),pixel)#COLOCAMOS LOS VALORES DE GRIS EN LOS PIXELES
+            print("Niveles de gris en la posicion ["+str(i)+" "+str(j)+"] = "+str(gris))
+            j += 1
+        i += 1   #AUMENTAMOS EN CADA ITERACIÓN PARA CAMBIAR CADA UNO DE LOS PIXELES
+    imgris.show()
+
+
     
