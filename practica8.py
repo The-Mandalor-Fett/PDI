aca3e376af5e50309926218e5ed3058c7327ebb9
The-Mandalor-Fett(David) <davidc.cortes@outlook.com>
2022-05-26 10:48:55 -0500

    Se agregó la función erosión

--- practica8.py
+++ practica8.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('A.png',0)
+
+img = cv2.imread("./img/estrella.jpg",0)
 kernel = np.ones((3,3),np.uint8)
-erosion = cv2.erode(img,kernel,iterations = 1)
+#Erosión
+imgErosion = cv2.erode(img,kernel,iterations = 1)
